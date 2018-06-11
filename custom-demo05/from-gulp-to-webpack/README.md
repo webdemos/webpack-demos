f9e887f409a01a11d4d9a29727282ca25bbc066f
chendachao <chendachao@outlook.com>
2018-06-11 12:35:26 +0800

    init

---
+++ custom-demo05/from-gulp-to-webpack/README.md
@@ -0,0 +1,6 @@
+
+
+https://www.valentinog.com/blog/from-gulp-to-webpack-4-tutorial/
+
+
+
---
+++ custom-demo05/from-gulp-to-webpack/webpack.config.js
@@ -0,0 +1,73 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: './img/[name].[ext]',
+              limit: 10000
+            }
+          },
+          {
+            loader: 'img-loader'
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: 'src/index.html',
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ]
+};
+
+
+
+
+
+
+
+
+
+
