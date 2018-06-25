bfd936cf3d0acb2597fc7ac4e1b11fec875a0b1f
chendachao <chendachao@outlook.com>
2018-06-25 12:32:11 +0800

    move to another repo

--- custom-demo05/from-gulp-to-webpack/src/index.js
+++
@@ -1,9 +0,0 @@
-console.log(`I've been required by Webpack`);
-import style from './_scss/main.scss';
-
-
-const arr = [1, 2, 3, 4];
-const iAmJavascriptES6 = () => console.log(...arr);
-window.iAmJavascriptES6 = iAmJavascriptES6;
-
-
